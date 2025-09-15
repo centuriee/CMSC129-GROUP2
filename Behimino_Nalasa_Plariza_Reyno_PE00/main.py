bfa18782e710df8a7a34e29fcbea3efc15756d8b
OneL-H <117259320+OneL-H@users.noreply.github.com>
2025-09-15 10:22:31 +0800

    reconverter + bugfixes
    - fixed incorrect postfix implementation (popped whole stack instead of checking each time)
    - added a reconverter to convert tokens back into strings

--- Behimino_Nalasa_Plariza_Reyno_PE00/main.py
+++ Behimino_Nalasa_Plariza_Reyno_PE00/main.py
@@ -1,5 +1,14 @@
 from lexer.lexer import Lexer
 
+def restring(token_stream):
+    # takes incoming token stream and converts back to readable string
+
+    token_values = []
+    for token in token_stream:
+        token_values.append(token.value)
+
+    return ' '.join(token_values)
+
 def postfixer(token_stream):
     
     tokens_out = []
@@ -23,20 +32,11 @@ def postfixer(token_stream):
         # do some checking if not
         elif token.type == "OPERATOR":
             
-            # empty stack, push as normal (prevents errors)
-            if len(operator_stack) == 0:
-                operator_stack.append(token)
-
-            # check precedence of highest element, if equal or greater pop until empty
-            elif precedence[operator_stack[len(operator_stack) - 1].value] >= precedence[token.value]:
-                while len(operator_stack) != 0:
-                    tokens_out.append(operator_stack.pop())
-                operator_stack.append(token)
-
-            # otherwise push as normal
-            else:
-                operator_stack.append(token)
-        
+            # check precedence of highest element, if greater pop. when not push.
+            while len(operator_stack) != 0 and precedence[operator_stack[len(operator_stack) - 1].value] >= precedence[token.value]:
+                tokens_out.append(operator_stack.pop())
+            operator_stack.append(token)
+    
         else:
             Exception("WARNING: Invalid token class passed to postfixer function.")
     
@@ -50,7 +50,13 @@ lexer = Lexer("1char=6")
 tokens = lexer.tokenize()
 print(tokens)
 
-example1 = Lexer("a+b*c+d")
+example1 = Lexer("a+b*c+d+e*f/g")
 tokens_1 = example1.tokenize()
+postfixed = postfixer(tokens_1)
 print(tokens_1)
-print(postfixer(tokens_1))
+print(postfixed)
+print(restring(postfixed))
+
+test1 = Lexer("(a+b)*c")
+tokens_2 = test1.tokenize()
+print(tokens_2)
